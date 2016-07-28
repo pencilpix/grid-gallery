1c0853414da4ae72776c8d7c1c62100cc5664852
Mohamed hassan <pencilpix11@gmail.com>
2016-07-28 04:48:02 +0200

    general notes about the plugin spec

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,3 +1,11 @@
+/*
+ * General specifications GridGallery
+ * 1- creates a grid using elements that will be in different heights and widths
+ * 2- will enable gallery view on click to an element of the grid (dialog)
+ * 3- the dialog will trigger custom events to control data in dialog
+ * 4- nav of items in grid (disable/enable) depending on initialization
+ */
+
 describe('test', function(){
  it('should be true', function() {
    expect(true).to.equal(true);
