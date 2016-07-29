4952afd705cb62c4e8b164969aa80a5c5dcdd479
Mohamed hassan <pencilpix11@gmail.com>
2016-07-29 12:44:09 +0200

    add the information header to the plugin

--- src/assets/js/grid-gallery.js
+++ src/assets/js/grid-gallery.js
@@ -1,1 +1,11 @@
+/*
+ * GridGallery is a plugin that will generate a grid depending on
+ * the elements width and height. it is not required to have the same height
+ * also you can enable/disable show lightbox effect to load content
+ *
+ * license: under MIT
+ *          https://github.com/pencilpix/grid-gallery/blob/master/LICENSE
+ * Author: Mohamed Hassan
+ * Author Url: mohamedhassan@me
+ */
 
