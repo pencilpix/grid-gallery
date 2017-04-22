a9cd353d3d09944c9bbb8bad52c49d7c01f4501b
Mohamed hassan <pencilpix11@gmail.com>
2017-04-22 02:39:27 +0200

    remove all old code

--- src/js/grid-gallery.js
+++ src/js/grid-gallery.js
@@ -1,247 +1,38 @@
-/*
- * @Description: GridGallery is a plugin that will generate a grid depending on
- *               the elements width and height. it is not required to have the same height
- *               also you can enable/disable show lightbox effect to load content
+/**
+ * @Description: GridGallery is a tiny package that will generate a grid depending on
+ *               the elements height. It is not required to have the same height
  *
- * @license:      under MIT
- *                https://github.com/pencilpix/grid-gallery/blob/master/LICENSE
- * @Author:       Mohamed Hassan
- * @Author Url:   http://mohamedhassan.me
+ * @Author:      Mohamed Hassan
+ * @Author Url:  http://mohamedhassan.me
+ *
+ * @License:     under MIT
+ *               https://github.com/pencilpix/grid-gallery/blob/master/LICENSE
  */
-(function($doc, $win){
-  'use strict';
+((name, definition) => {
+  let theModule  = definition(),
+      hasDefine  = typeof define === 'function' && define.amd,
+      hasExports = typeof module !== 'undefined' && module.exports;
 
-  const DEFAULTS = { direction: 'left' };
-  const VERSION = '1.0.1-alpha';
+  if(hasDefine)
+    define(theModule);
+  else if(hasExports)
+    module.exports = theModule;
+  else
+    window[name] = theModule;
+
+})('GridGallery', () => {
+  const VERSION = '1.0.1';
+
+  const DEFAULTS = {};
 
 
-  /*
-   * class constructor of GridGallery component
-   * @param { HTMLElement } element The element that will be the container.
-   * @param { Object } options The options to initialize the component.
-   */
+
   class GridGallery {
-    constructor(element, options) {
+    constructor() {
 
-      this.element = element;
-
-      this.options = Object.assign({}, DEFAULTS, options); // extend options to default
-      this._resizeTimeout = null;
-      this._bindResizeHandler = this._resizeHandler.bind(this);
-
-
-
-      this.init();
-    }
-
-    /**
-     * PUBLIC
-     * ===================================*/
-
-    /*
-     * init this method that do the rest of initialization
-     * work and updates the dom.
-     */
-    init() {
-      this._enableItems(this._checkItems(this.element.children));
-
-      // listen to resize and observe dom changes
-      // if any item is inserted via ajax ... etc.
-      window.addEventListener('resize', this._bindResizeHandler);
-      this._createObserver(this, this.element, this.update);
-    }
-
-
-    update() {
-      this._disableAll(this.element.querySelectorAll('.grid-gallery__item'));
-      this._enableItems(this._checkItems(this.element.children));
-    }
-
-
-
-    /**
-     * Private
-     * =========================================*/
-
-    /**
-     * create an observer for dom element and invoke callback it the change is
-     * a node list.
-     * @param  { Object }      context   the Object that will be used in explicit call
-     * @param  { HTMLElement } target    the element that it should be observed
-     * @param  { Function }    callback  function that will run when DOM changed
-     * @return { Object }                observer
-     */
-    _createObserver(context, target, callback) {
-      // MutationObserver compatability.
-      var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-          eventListenerSupported = window.addEventListener;
-
-      // handler if MutationObserver is not supported
-      function _handler() {
-        callback.call(context);
-      }
-
-      if(MutationObserver) {
-        // create observer.
-        var observer = new MutationObserver(function(mutations) {
-          if(mutations[0].addedNodes.length || mutations[0].removedNodes.length){
-            callback.call(context);
-          }
-        });
-
-        // configuration of the observer:
-        var config = {childList: true};
-
-        // pass in the target node, as well as the observer options
-        observer.observe(target, config);
-      } else if(eventListenerSupported) {
-        target.addEventListener('DOMNodeInserted', _handler, false);
-        target.addEventListener('DOMNodeRemoved', _handler, false);
-      }
-    }
-
-
-
-    /*
-     * _checkItems Private method that check if GridGallery enabled on items
-     * @param { Array-like } items The grid elements.
-     * @return { Array } notEnabled The grid items that not set yet.
-     */
-    _checkItems(items) {
-      var notEnabled = Array.prototype.filter.call(items, function(item) {
-        var pattern = new RegExp('grid-gallery__item');
-        if(!item.dataset.grid && pattern.test(item.className)){
-          return item;
-        }
-      });
-
-      if (notEnabled.length){
-        return notEnabled;
-      }
-    }
-
-
-
-    /**
-     * disables all items via data-grid
-     * @param  { Array-like } items grid items
-     * @return { Array }       array of items after clearing data-grid.
-     */
-    _disableAll(items) {
-      return Array.prototype.map.call(items, function(item){
-        item.dataset.grid = '';
-        return item;
-      });
-    }
-
-
-
-    /**
-     * method that return the whitespace after get the available number of
-     * items will be distributed through the parent width
-     * @param  { Number } parentWidth width of the container
-     * @param  { Number } itemWidth the item width
-     * @return { Number } whiteSpace.
-     */
-    _calWhiteSpace (parentWidth, itemWidth) {
-      var itemsNo = Math.floor(parentWidth / itemWidth);
-      return parentWidth - itemWidth * itemsNo;
-    }
-
-
-
-    /*
-     * _enableItems Private method to handle un-enabled grid items
-     * and give each element position and data-grid attribute.
-     * @param { Array } items un-enabled elements.
-     * @retrun { Array } enabled elements after update position
-     * and data-grid.
-     */
-    _enableItems(items) {
-      var parentPos = items[0].parentNode.getBoundingClientRect();
-      var rowItemsNo = Math.floor(parentPos.width / items[0].offsetWidth);
-      var nextPos = [];
-      var prevItem, parentHeight;
-      var whiteSpace = this._calWhiteSpace(parentPos.width, items[0].offsetWidth);
-
-      // enable elements and set each item position
-      var enabled = items.map(function(item, index, ar) {
-       if(index === 0) {
-         item.dataset.top = 0;
-         item.dataset.left = whiteSpace / 2;
-
-         item.style.top = 0;
-         item.style.left = whiteSpace / 2 + 'px';
-
-         nextPos[0] = {
-           top: item.offsetHeight,
-           left: 0
-         }
-       } else if(index < rowItemsNo) {
-         prevItem = ar[index - 1];
-
-         item.dataset.top = 0;
-         item.dataset.left = parseInt(prevItem.dataset.left) + prevItem.offsetWidth;
-
-         item.style.top = 0;
-         item.style.left = parseInt(prevItem.dataset.left) + prevItem.offsetWidth + 'px';
-
-         nextPos[index] = {
-           top: item.offsetHeight,
-           left: parseInt(item.dataset.left)
-         };
-       } else if(index % rowItemsNo <= nextPos.length){
-         var posIndex = index % rowItemsNo;
-
-         item.dataset.top = nextPos[posIndex].top;
-         item.dataset.left = (posIndex) ? nextPos[posIndex].left : (whiteSpace / 2);
-
-         item.style.top = nextPos[posIndex].top + 'px';
-         item.style.left = (posIndex) ? nextPos[posIndex].left + 'px' : (whiteSpace / 2) + 'px';
-
-         nextPos[posIndex] = {
-           top: parseInt(item.dataset.top) + item.offsetHeight,
-           left: parseInt(item.dataset.left)
-         };
-       }
-
-       item.dataset.grid = true;
-      });
-
-      // set container height
-      parentHeight = nextPos.reduce(function(prevPos, pos) {
-        if(prevPos > pos.top) {
-          return prevPos;
-        } else {
-          prevPos = pos.top;
-          return prevPos;
-        }
-      }, nextPos[0].top);
-
-      items[0].parentNode.style.height = parentHeight + 'px';
-      return enabled;
-    }
-
-
-    _resizeHandler() {
-      clearTimeout(this._resizeTimeout);
-
-      this._resizeTimeout = setTimeout(() => {
-        this.update();
-      }, 250);
     }
   }
 
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = GridGallery;
-  } else {
-    if (typeof define === 'function' && define.amd) {
-      define([], function() {
-        return GridGallery;
-      });
-    } else {
-      $win.GridGallery = GridGallery;
-    }
-  }
+  return GridGallery;
+});
 
-})(document, window);
