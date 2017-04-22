3423eddd9baa495e1158d1dfb5bc4e6085a2d279
Mohamed hassan <pencilpix11@gmail.com>
2017-04-22 14:42:29 +0200

    Generate grid rows & Update each item position
    - [x] get rows using `updateGridRows` depending on items in the grid container
    - [x] get item position depending on it's index in the grid-gallery's grid
    - [x] updating positions of elements after finishing the total grid info.

--- src/js/grid-gallery.js
+++ src/js/grid-gallery.js
@@ -33,12 +33,108 @@
     constructor( element, options ) {
       this.element = element;
       this.options = Object.assign({}, GridGallery.DEFAULTS, options);
+      this.rows = [];
+
+      this.init();
+    }
+
+
+
+    init() {
+      this.updateGridRows();
+      this.updatePositions();
+    }
+
+
+
+    /**
+     * get rows of the grid depending on current
+     * container children and it's order
+     */
+    updateGridRows() {
+      [...this.element.children].forEach((item, i) => {
+        let index = i % this.maxItemsPerRow;
+        let position;
+        if ( index === 0){
+          this.rows.push([]);
+        }
+
+        position = this.getItemPosition(index);
+        this.rows[this.lastIndex].push({item: item, position: position});
+      });
+    }
+
+
+    /**
+     * calculate the position of an element depending on it's
+     * position on the grid.
+     *
+     * @param {Number}  index  the number reperesent index in current row
+     * @return {Object}
+     */
+    getItemPosition(index) {
+      let direction = this.options.direction;
+      let lastIndex = this.lastIndex;
+      let rowIs     = lastIndex === 0 ? 'first_row' : 'any_row';
+      let itemIs    = index === 0 ? 'first_item' : 'any_item';
+
+      let position  = {};
+      let prevItem, prevRowItem;
+
+
+      switch (rowIs + '_and_' + itemIs) {
+        case 'first_row_and_first_item':
+          position.top = 0;
+          position[direction] = this.remainSpace;
+          break;
+
+        case 'first_row_and_any_item':
+          prevItem = this.rows[lastIndex][index - 1];
+          position.top = 0;
+          position[direction] = prevItem.position[direction] + prevItem.item.offsetWidth;
+          break;
+
+        case 'any_row_and_first_item':
+          prevRowItem = this.rows[lastIndex - 1][index];
+          position.top = prevRowItem.position.top + prevRowItem.item.offsetHeight;
+          position[direction] = this.remainSpace;
+          break;
+
+        case 'any_row_and_any_item':
+          prevItem = this.rows[lastIndex][index - 1];
+          prevRowItem = this.rows[lastIndex - 1][index];
+          position.top = prevRowItem.position.top + prevRowItem.item.offsetHeight;
+          position[direction] = prevItem.position[direction] + prevItem.item.offsetWidth;
+          break;
+      }
+
+      return position;
+    }
+
+
+
+    /**
+     * update the position of each item
+     * depending on position iformation
+     * stored in item object of in rows property
+     */
+    updatePositions() {
+      let direction = this.options.direction;
+      if(this.lastIndex === -1)
+        return;
+
+      this.rows.forEach(row => {
+        row.forEach(rowItem => {
+          rowItem.item.style.top = rowItem.position.top + 'px';
+          rowItem.item.style[direction] = rowItem.position[direction] + 'px';
+        });
+      });
     }
 
 
 
     get itemWidth() {
-      return this.element.querySelector('.grid-gallery__item').offsetWidth;
+      return this.element.children[0].offsetWidth;
     }
 
 
@@ -53,6 +149,14 @@
       let itemsWidth = this.maxItemsPerRow * this.itemWidth
 
       return Math.floor((containerWidth - itemsWidth) / 2);
+    }
+
+
+    /**
+     * get last row index in grid
+     */
+    get lastIndex() {
+      return this.rows.length - 1;
     }
 
 
